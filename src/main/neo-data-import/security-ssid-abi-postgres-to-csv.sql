4c21db4e891fd0f1d953bbf004c11381a06d3d64
gisdev01 <gisdev01@gmail.com>
2020-05-20 18:03:43 -0400

    New commands for copying postgres-exported CSVs to local host OS

--- src/main/neo-data-import/security-ssid-abi-postgres-to-csv.sql
+++ src/main/neo-data-import/security-ssid-abi-postgres-to-csv.sql
@@ -1,7 +1,11 @@
-COPY (SELECT * FROM public.security_ssid_ap) TO '/tmp/security_ssid_ap_1.csv' WITH CSV header;
-COPY (SELECT * FROM public.security_ssid_client) TO '/tmp/security_ssid_client_1.csv' WITH CSV header;
+COPY (SELECT *
+      FROM public.security_ssid_ap) TO '/tmp/security_ssid_ap_1.csv' WITH CSV header;
+COPY (SELECT *
+      FROM public.security_ssid_client) TO '/tmp/security_ssid_client_1.csv' WITH CSV header;
 
 -- One-to-many join table of IDs from AP->Client
-COPY (SELECT * FROM public.security_ssid_ap_client) TO '/tmp/security_ssid_ap_client_1.csv' WITH CSV header;
+COPY (SELECT *
+      FROM public.security_ssid_ap_client) TO '/tmp/security_ssid_ap_client_1.csv' WITH CSV header;
 
-
+-- docker cp <containerId>:/tmp/filename /host/path/dir
+-- docker cp c386df595c73:/tmp/ ~/neo4j/import
